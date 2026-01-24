87e2400910a3fdb133dd878fdf58ebe19b1badd8
LaVillainTMM <LavailaDrain@gmail.com>
2026-01-24 15:53:09 -0500

    Delete firebase_client.py

--- firebase_client.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-FIREBASE_URL = "https://athenaai-memory-default-rtdb.firebaseio.com"
-
-def send(path, data):
-    url = f"{FIREBASE_URL}/{path}.json"
-    return requests.post(url, json=data)
