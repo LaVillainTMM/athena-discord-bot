2d2bdca9f813d3780ffd2dc5098f543cd0f59a55
LaVillainTMM <LavailaDrain@gmail.com>
2026-03-16 10:29:27 -0400

    Update athenaGoalManager.js

--- strategy/athenaGoalManager.js
+++ strategy/athenaGoalManager.js
@@ -3,21 +3,13 @@ import { firestore } from "../firebase.js";
 
 const db = firestore;
 
-export async function createGoal(goalData) {
-  const res = await db.collection("athena_goals")
-      .add(goalData);
-
-const db = firestore;
-
 export async function createGoal(goalText) {
 
     await db.collection("athena_goals").add({
-
         goal: goalText,
         priority: Math.random(),
         createdAt: new Date(),
         status: "active"
-
     });
 
 }
