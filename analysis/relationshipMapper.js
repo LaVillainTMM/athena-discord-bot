a680b7dc56d5554dd841991de9ed1a6566a608d9
LaVillainTMM <LavailaDrain@gmail.com>
2026-03-16 10:25:53 -0400

    Update relationshipMapper.js

--- analysis/relationshipMapper.js
+++ analysis/relationshipMapper.js
@@ -1,15 +1,9 @@
 // File: analysis/relationshipMapper.js
 import { firestore } from "../firebase.js";
 
-export async function mapRelationships(channelId, username) {
-  const db = firestore;
-  const snapshot = await db.collection("messages")
-      .where("channelId", "==", channelId)
-      .orderBy("timestamp", "desc")
-      .limit(100)
-      .get();
-  
 export async function mapRelationships() {
+
+    const db = firestore;
 
     const snapshot = await db.collection("messages").get();
 
